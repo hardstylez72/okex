9591717368c83344d23826742896a3cf0c13f682
hs <korotkovcv77@gmail.com>
2023-05-30 23:09:31 +0700

    fixes http.client injection

--- requests/rest/funding/funding_requests.go
+++ requests/rest/funding/funding_requests.go
@@ -38,8 +38,8 @@ type (
 		Chain  string                     `json:"chain,omitempty"`
 		ToAddr string                     `json:"toAddr"`
 		Pwd    string                     `json:"pwd"`
-		Amt    float64                    `json:"amt,string"`
-		Fee    float64                    `json:"fee,string"`
+		Amt    string                     `json:"amt,string"`
+		Fee    string                     `json:"fee,string"`
 		Dest   okex.WithdrawalDestination `json:"dest,string"`
 	}
 	GetWithdrawalHistory struct {
